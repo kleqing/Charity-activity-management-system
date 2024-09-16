7985cb1d3aaf209ff22d85d73b42df115dabef92
Pham Khanh Nhan <78801337+kleqing@users.noreply.github.com>
2024-09-16 21:37:50 +0700

    Add UserDAO

---
+++ DataAccess/UserDAO.cs
@@ -0,0 +1,57 @@
+﻿using Charity_Management_System;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class UserDAO : SingleOnBase<UserDAO>
+    {
+        // Get all users
+        public async Task<IEnumerable<User>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+        // Find user by id
+        public async Task<User> GetUserById(int id)
+        {
+            _context = new DBContext();
+            var userID = await _context.Users.FirstOrDefaultAsync(u => u.userID == id);
+            if (userID == null)
+            {
+                return null;
+            }
+            return userID;
+        }
+        // Add user
+        public async Task AddUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+        // Update user
+        public async Task UpdateUser(User user)
+        {
+            var existingItem = await GetUserById(user.userID);
+            if (existingItem == null)
+            {
+                return;
+            }
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+        // Delete user
+        public async Task DeleteUser(int id)
+        {
+            var user = await GetUserById(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
