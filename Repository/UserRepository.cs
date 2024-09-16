d800e7b24836abf446078d9a5a4f0407d5e6c70c
Pham Khanh Nhan <78801337+kleqing@users.noreply.github.com>
2024-09-16 21:38:11 +0700

    Add User Repository and it's interface

---
+++ Repository/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using Charity_Management_System;
+using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        public async Task AddUser(User user)
+        {
+            await UserDAO.Instance.AddUser(user);
+        }
+
+        public async Task DeleteUser(int id)
+        {
+            await UserDAO.Instance.DeleteUser(id);
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsers()
+        {
+            return await UserDAO.Instance.GetAllUsers();
+        }
+
+        public async Task<User> GetUserById(int id)
+        {
+            return await UserDAO.Instance.GetUserById(id);
+        }
+
+        public async Task UpdateUser(User user)
+        {
+            await UserDAO.Instance.UpdateUser(user);
+        }
+    }
+}
