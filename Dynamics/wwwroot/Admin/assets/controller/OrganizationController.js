7d9f02794a81f2bc8e755841455a5eeb68e5fd93
Pham Khanh Nhan <78801337+kleqing@users.noreply.github.com>
2024-10-16 22:39:04 +0700

    Update js (ajax)

--- Dynamics/wwwroot/Admin/assets/controller/OrganizationController.js
+++ Dynamics/wwwroot/Admin/assets/controller/OrganizationController.js
@@ -3,28 +3,36 @@
         common.registerEvent();
     },
     registerEvent: function () {
-        $('.btn-active').off('click').on('click', function (e) {
+        $('.btn-status').off('click').on('click', function (e) {
             e.preventDefault();
             var btn = $(this);
             var id = btn.data('id');
+            var currentStatus = parseInt(btn.data('status'));
+
+            var newStatus = currentStatus === 1 ? -1 : 1;
+
             $.ajax({
-                url: "/Admin/Organizations/BanOrganization",
-                data: { id: id },
+                url: "/Admin/Organizations/ChangeStatus",
+                data: { id: id, status: newStatus },
                 datatype: "json",
                 type: "POST",
                 success: function (response) {
-                    console.log(response);
-                    if (response.isBanned == true) {
-                        btn.text('Banned');
-                        btn.removeClass('badge badge-success-lighten').addClass('badge badge-danger-lighten');
+                    if (response.status === 1) {
+                        btn.text('Active')
+                            .removeClass('badge-warning-lighten badge-danger-lighten')
+                            .addClass('badge-success-lighten');
+                    } else if (response.status === -1) {
+                        btn.text('Banned')
+                            .removeClass('badge-warning-lighten badge-success-lighten')
+                            .addClass('badge-danger-lighten');
                     }
-                    else {
-                        btn.text('Active');
-                        btn.removeClass('badge badge-danger-lighten').addClass('badge badge-success-lighten');
-                    }
+
+                    // Cập nhật status mới vào data-status
+                    btn.data('status', newStatus);
                 }
             });
         });
     }
+
 }
 common.init();
