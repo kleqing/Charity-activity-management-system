f2ecffa1f2e95d77376c8aa7228fbd38c2cc48c0
Pham Khanh Nhan <78801337+kleqing@users.noreply.github.com>
2024-10-16 22:20:58 +0700

    update migrations

---
+++ Dynamics.DataAccess/Migrations/20241016150618_Rename organization status.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dynamics.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Renameorganizationstatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isBanned",
+                table: "Organizations");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrganizationStatus",
+                table: "Organizations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrganizationStatus",
+                table: "Organizations");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isBanned",
+                table: "Organizations",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
